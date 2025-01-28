aa2f1da54a4d52d4b2983ce60f157ea9c147a278
HamzaAhmedSheikh <hamza.ahmed@scrift.io>
2025-01-29 00:46:40 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,10 +6,22 @@ import router from './routes/dalle.routes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use('/api/v1/dalle', router)
+
+
+// Configure CORS options
+const corsOptions = {
+    origin: 'https://aicustomshirtbuilder.vercel.app', // Allow only this origin
+    methods: ['GET', 'POST'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Allow cookies to be sent
+  };
+  
+  // Apply CORS middleware
+app.use(cors(corsOptions));
 
 app.get('/' , (req, res) => {
     res.status(200).json({message: "Hello from dalle"})
